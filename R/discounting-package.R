ef4374d20c69211882cb8a3e372628c392a18bf0
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2026-01-21 17:21:32 +0100

    Add package file

---
+++ R/discounting-package.R
@@ -0,0 +1,16 @@
+#' @description
+#' Functions created to aid with a large-scale evaluation of discounting 
+#' models for capturing affect in experimental and daily-life datasets. 
+#' Introduces a class for each separate model as well as a class for datasets,
+#' making every function internal to the package run within a standardized 
+#' framework
+#' 
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import MASS
+#' @importFrom methods new
+#' @importFrom utils head
+## usethis namespace: end
+NULL
