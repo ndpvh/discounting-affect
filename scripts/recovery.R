51c1198b3a4e31199919cd66561eaffebb0064bb
ndpvh <niels.vanhasbroeck@gmail.com>
2026-01-24 08:28:58 +0100

    Delete break at end of recovery

--- scripts/recovery.R
+++ scripts/recovery.R
@@ -15,7 +15,7 @@ devtools::load_all()
 # PRELIMINARIES
 
 # Define the number of recoveries `iterations` and the number of datapoints `N`
-iterations <- 10
+iterations <- 100
 N <- 50
 
 # Define the models to use for the recovery study
@@ -75,179 +75,190 @@ fx <- list(
 ################################################################################
 # PERFORM THE RECOVERY
 
-# Loop over the different models
-for(i in seq_along(models)) {
-    # Extract the model of choice
-    my_model <- models[[i]]
+# Loop over the different models. Perform this in parallel on Mac and Ubuntu
+# and in sequence on Windows
+empty <- parallel::mclapply(
+    seq_along(models),
+    function(i) {
+        # Extract the model of choice
+        my_model <- models[[i]]
 
-    # Determine which of the functions for X will serve as the one in this 
-    # recovery
-    Xfun <- x_functions[[as.integer(my_model@k)]]
+        # Determine which of the functions for X will serve as the one in this 
+        # recovery
+        Xfun <- x_functions[[as.integer(my_model@k)]]
 
-    # Loop over the different characteristics of the models
-    for(j in seq_len(nrow(characteristics))) {
-        # Perform the recovery study
-        result <- recovery(
-            my_model,
-            iterations = iterations,
-            fx = fx,
+        # Loop over the different characteristics of the models
+        for(j in seq_len(nrow(characteristics))) {
+            # Perform the recovery study
+            result <- recovery(
+                my_model,
+                iterations = iterations,
+                fx = fx,
 
-            # Simulation characteristics
-            Xfun = Xfun,
-            N = N,
+                # Simulation characteristics
+                Xfun = Xfun,
+                N = N,
 
-            # Model characteristics
-            dynamics = characteristics[j, 1],
-            covariance = characteristics[j, 2],
+                # Model characteristics
+                dynamics = characteristics[j, 1],
+                covariance = characteristics[j, 2],
 
-            # DEoptim characteristics
-            itermax = 10,
-            strategy = 6,
-            p = 0.5, 
-            NP = 200,
-            trace = FALSE,
+                # DEoptim characteristics
+                itermax = 1000,
+                strategy = 6,
+                p = 0.5, 
+                NP = 200,
+                trace = FALSE,
 
-            # Additional stuff
-            print_iteration = TRUE,
-            print_content = paste(
-                names(models)[i], 
-                ": ",
-                characteristics[j, 1], 
-                " - ",
-                characteristics[j, 2],
-                ": ",
-                sep = ""
-            )
-        )
-
-        # Save the result
-        saveRDS(
-            result,
-            file.path(
-                "scripts", 
-                "results", 
-                "recovery",
-                paste0(
+                # Additional stuff
+                print_iteration = TRUE,
+                print_content = paste(
                     names(models)[i], 
-                    "__", 
+                    ": ",
                     characteristics[j, 1], 
-                    "_",
-                    characteristics[j, 2], 
-                    ".Rds"
+                    " - ",
+                    characteristics[j, 2],
+                    ": ",
+                    sep = ""
                 )
             )
-        )
 
-        # Create recovery plots
-        plt <- lapply(
-            colnames(result$simulate),
-            function(col) {
-                # Create a dataframe of simulated parameters values (x) and 
-                # estimated ones (y)
-                plot_data <- cbind(
-                    result$simulate[, col],
-                    result$fit[, col]
-                ) |>
-                    as.data.frame() |>
-                    `colnames<-` (c("x", "y"))
-
-                # Get the range of values
-                limits <- range(c(plot_data$x, plot_data$y))
-
-                # Create a plot for the recovery
-                plt <- ggplot2::ggplot(
-                    data = plot_data, 
-                    ggplot2::aes(
-                        x = x, 
-                        y = y
+            # Save the result
+            saveRDS(
+                result,
+                file.path(
+                    "scripts", 
+                    "results", 
+                    "recovery",
+                    paste0(
+                        names(models)[i], 
+                        "__", 
+                        characteristics[j, 1], 
+                        "_",
+                        characteristics[j, 2], 
+                        ".Rds"
                     )
-                ) +
-                    ggplot2::geom_abline(
-                        intercept = 0, 
-                        slope = 1, 
-                        linewidth = 2, 
-                        color = "black"
+                )
+            )
+
+            # Create recovery plots
+            plt <- lapply(
+                colnames(result$simulate),
+                function(col) {
+                    # Create a dataframe of simulated parameters values (x) and 
+                    # estimated ones (y)
+                    plot_data <- cbind(
+                        result$simulate[, col],
+                        result$fit[, col]
+                    ) |>
+                        as.data.frame() |>
+                        `colnames<-` (c("x", "y"))
+
+                    # Get the range of values
+                    limits <- range(c(plot_data$x, plot_data$y))
+
+                    # Create a plot for the recovery
+                    plt <- ggplot2::ggplot(
+                        data = plot_data, 
+                        ggplot2::aes(
+                            x = x, 
+                            y = y
+                        )
                     ) +
-                    ggplot2::geom_point(
-                        shape = 21,
-                        alpha = 0.25, 
-                        color = "black",
-                        fill = "cornflowerblue",
-                        size = 5
-                    ) +
-                    ggplot2::annotate(
-                        "text",
-                        x = limits[1] + 0.05 * diff(limits),
-                        y = limits[1] + 0.95 * diff(limits),
-                        label = format(
-                            round(
-                                cor(
-                                    plot_data$x, 
-                                    plot_data$y
-                                ),
-                                digits = 2
-                            ),
-                            nsmall = 2
-                        ),
-                        size = 5,
-                        hjust = 0
-                    ) +
-                    ggplot2::lims(
-                        x = limits,
-                        y = limits
-                    ) +
-                    ggplot2::labs(
-                        title = col,
-                        x = "Simulated",
-                        y = "Estimated"
-                    ) +
-                    ggplot2::theme(
-                        panel.background = ggplot2::element_rect(
-                            fill = "white"
-                        ),
-                        panel.border = ggplot2::element_rect(
-                            fill = NA, 
+                        ggplot2::geom_abline(
+                            intercept = 0, 
+                            slope = 1, 
+                            linewidth = 2, 
+                            color = "black"
+                        ) +
+                        ggplot2::geom_point(
+                            shape = 21,
+                            alpha = 0.25, 
                             color = "black",
-                            linewidth = 1
-                        ),
-                        axis.title = ggplot2::element_text(size = 15),
-                        plot.title = ggplot2::element_text(size = 20)
-                    )
-                
-                return(plt)
+                            fill = "cornflowerblue",
+                            size = 5
+                        ) +
+                        ggplot2::annotate(
+                            "text",
+                            x = limits[1] + 0.05 * diff(limits),
+                            y = limits[1] + 0.95 * diff(limits),
+                            label = format(
+                                round(
+                                    cor(
+                                        plot_data$x, 
+                                        plot_data$y
+                                    ),
+                                    digits = 2
+                                ),
+                                nsmall = 2
+                            ),
+                            size = 5,
+                            hjust = 0
+                        ) +
+                        ggplot2::lims(
+                            x = limits,
+                            y = limits
+                        ) +
+                        ggplot2::labs(
+                            title = col,
+                            x = "Simulated",
+                            y = "Estimated"
+                        ) +
+                        ggplot2::theme(
+                            panel.background = ggplot2::element_rect(
+                                fill = "white"
+                            ),
+                            panel.border = ggplot2::element_rect(
+                                fill = NA, 
+                                color = "black",
+                                linewidth = 1
+                            ),
+                            axis.title = ggplot2::element_text(size = 15),
+                            plot.title = ggplot2::element_text(size = 20)
+                        )
+                    
+                    return(plt)
+                }
+            )
+            if(length(plt) %% 2 == 1) {
+                plt[[length(plt) + 1]] <- ggplot2::ggplot() +
+                    ggplot2::theme_void()
             }
-        )
-        if(length(plt) %% 2 == 1) {
-            plt[[length(plt) + 1]] <- ggplot2::ggplot() +
-                ggplot2::theme_void()
+
+            plt <- cowplot::plot_grid(
+                plotlist = plt,
+                nrow = my_model@d,
+                ncol = round(length(plt) / my_model@d),
+                byrow = FALSE
+            )
+
+            ggplot2::ggsave(
+                file.path(
+                    "scripts",
+                    "figures",
+                    "recovery",
+                    paste0(
+                        names(models)[i], 
+                        "__", 
+                        characteristics[j, 1], 
+                        "_",
+                        characteristics[j, 2], 
+                        ".png"
+                    )
+                ),
+                plt,
+                width = ceiling(ncol(result$simulate) * 1500 / my_model@d), 
+                height = my_model@d * 1550,
+                unit = "px",
+                limitsize = FALSE
+            )
         }
 
-        plt <- cowplot::plot_grid(
-            plotlist = plt,
-            nrow = my_model@d,
-            ncol = round(length(plt) / my_model@d),
-            byrow = FALSE
-        )
-
-        ggplot2::ggsave(
-            file.path(
-                "scripts",
-                "figures",
-                "recovery",
-                paste0(
-                    names(models)[i], 
-                    "__", 
-                    characteristics[j, 1], 
-                    "_",
-                    characteristics[j, 2], 
-                    ".png"
-                )
-            ),
-            plt,
-            width = ncol(result$simulate) * 1500, 
-            height = my_model@d * 1650,
-            unit = "px",
-            limitsize = FALSE
-        )
-    }
-}
+        return(NULL)
+    },
+    mc.cores = ifelse(
+        Sys.info()["sysname"] == "Windows",
+        1,
+        parallel::detectCores() - 1
+    )
+)
